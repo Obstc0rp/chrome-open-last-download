9e28c9cf3feab2880d72eb4c56c6fe60440a63f1
Obstc0rp <wolf.scholle@gmail.com>
2015-01-29 15:53:02 +0100

    added new command "open download dir"

--- extension/assets/js/background.js
+++ extension/assets/js/background.js
@@ -3,11 +3,22 @@
  * 
  */
  
+ var openLastDownload = 'open-last-download';
+ var openDownloadDir = 'open-download-dir';
+
 /**
  * Add a listener to chrome commands.
  */
 chrome.commands.onCommand.addListener(function(command) {
-	chrome.downloads.search({}, function(results){	//search for all DownloadItems	
-		chrome.downloads.open(results[results.length-1].id);	//open the last DownloadItem
-	});
+
+	//open last download
+	if(command == openLastDownload){
+		chrome.downloads.search({}, function(results){	//search for all DownloadItems	
+			
+			if(results[results.length-1].id)
+				chrome.downloads.open(results[results.length-1].id);	//open the last DownloadItem
+		});
+	} else if(command == openDownloadDir) {		//open default download directory
+		chrome.downloads.showDefaultFolder();
+	}
 });
